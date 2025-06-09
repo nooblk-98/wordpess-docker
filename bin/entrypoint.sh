ebe247aa9a0780863db3e2b844a1a3eb83d0ad3a
Lahiru Liyanage <75250151+weblankanteam@users.noreply.github.com>
2025-06-09 08:16:05 +0530

    update upload_max_filesize chnage ability

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -7,6 +7,11 @@ set -e
 # wordpress init 
 /usr/local/bin/init.sh
 
+# Set PHP upload and memory limits
+echo "upload_max_filesize=${PHP_UPLOAD_MAX_FILESIZE:-64M}" > /usr/local/etc/php/conf.d/uploads.ini
+echo "post_max_size=${PHP_POST_MAX_SIZE:-64M}" >> /usr/local/etc/php/conf.d/uploads.ini
+echo "memory_limit=${PHP_MEMORY_LIMIT:-512M}" > /usr/local/etc/php/conf.d/memory-limit.ini
+
 # Fix wordperss permissions
 chown -R  www-data:www-data /var/www/html
 
